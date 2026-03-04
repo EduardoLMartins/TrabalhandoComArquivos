ab20a38aced56e4a877626f41bad04bafe4cdcd6
EduardoLMartins <eduardol_martins@hotmail.com>
2026-03-04 18:53:15 -0300

    Bloco try with resources

--- src/main/java/org/example/FileReaderBufferredReader.java
+++ src/main/java/org/example/FileReaderBufferredReader.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -10,13 +9,8 @@ public class FileReaderBufferredReader {
 
 
         String path = ("P:\\Udemy_Nelio_Alves\\TrabalhandoComArquivos\\in.txt");
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
 
             while (line != null) {
@@ -26,17 +20,6 @@ public class FileReaderBufferredReader {
 
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
